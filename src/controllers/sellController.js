c55782a8aa48dd35a27fbf9bbc696f3c9c2463df
Patricio Pittana <pittanapatricio@gmail.com>
2024-12-07 18:41:58 -0300

    sellController updated

--- src/controllers/sellController.js
+++ src/controllers/sellController.js
@@ -9,7 +9,7 @@ class sellController {
         return res.status(400).json({ error: 'Debe incluir al menos un producto.' });
       }
 
-      const productosInvalidos = products.filter(p => !p.productId || !p.quantity || !p.price);
+      const productosInvalidos = products.filter(p => !p.productId || !p.quantity || !p.salePrice || !p.buyPrice);
       if (productosInvalidos.length > 0) {
         return res.status(400).json({ error: 'Los productos deben tener productId, quantity, sellPrice y buyPrice.' });
       }
