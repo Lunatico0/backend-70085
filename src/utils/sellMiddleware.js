ec6d5b851705dc5609fb2d0ff3233fc560f07538
Patricio Pittana <pittanapatricio@gmail.com>
2024-12-07 18:49:22 -0300

    saleMiddleware updated

--- src/utils/sellMiddleware.js
+++ src/utils/sellMiddleware.js
@@ -5,9 +5,9 @@ const validarVenta = (req, res, next) => {
     return res.status(400).json({ error: 'Debe incluir al menos un producto.' });
   }
 
-  const productosInvalidos = products.filter(p => !p.productId || !p.quantity || !p.price);
+  const productosInvalidos = products.filter(p => !p.productId || !p.quantity || !p.salePrice || !p.buyPrice);
   if (productosInvalidos.length > 0) {
-    return res.status(400).json({ error: 'Los productos deben tener productId, quantity y price.' });
+    return res.status(400).json({ error: 'Los productos deben tener productId, quantity, sellPrice y buyPrice.' });
   }
 
   next();
